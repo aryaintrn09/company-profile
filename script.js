51e74e91904b048c079fad76cb8f738c8df62088
Arya Ngurah Intaran <aryaintaran09@proton.me>
2023-12-22 23:38:06 +0800

    FINISH

--- script.js
+++ script.js
@@ -34,3 +34,23 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
+// DARK LIGHT MODE
+let darkModeIcon = document.querySelector('#darkmode');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+}
+
+// SCROLL REVEAL
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .service-container, form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
